b8ee4058520f7719e9f24ea3e3abe0b5df448b60
Honoroshi <se.soravich_st@tni.ac.th>
2023-09-16 10:37:37 +0700

    First Commit [September-16-2023]

--- App.js
+++ App.js
@@ -1,62 +1,92 @@
-import { View, Text, Button } from 'react-native'
+import 'react-native-gesture-handler';
+import { View, Text, Linking } from 'react-native'
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native' // For calling other navigation and switching other page
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from './screens/HomeScreen'
-import DetailsScreen from './screens/DetailsScreen'
-// import FirstPage from './pages/FirstPage'
-// import SecondPage from './pages/SecondPage'
-// import ThirdPage from './pages/ThirdPage'
 
-//  function HomeScreen({navigation}) { /*Build Function*/ 
-//   return(
-//     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title='Go to Details'
-//         onPress={()=>navigation.navigate('Details')}
-//         />
-//     </View>
-//   )
-// }
+import {NavigationContainer} from '@react-navigation/native'; //we will use Navigation
+import {createDrawerNavigator} from '@react-navigation/drawer'; //we will use Navigation-Drawer Navigation
+import { DrawerContentScrollView,
+         DrawerItemList,
+         DrawerItem
+        } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// function DetailsScreen() {
-//   return(
-//     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-//       <Text>Detail Screen</Text>
-//     </View>
-//   )
-// }
+import FirstPage from "./pages/FirstPage";
+import SecondPage from "./pages/SecondPage";
+import ThirdPage from "./pages/ThirdPage";
 
-const stack = createNativeStackNavigator(); //The way easy use stack library in return()
+import CustomSideBarMenu from './pages/CustomSideBarMenu';
+
+function FirstScreenStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        // headerStyle: { backgroundColor: "#FFCEAD" },
+        // headerTintColor: "#025CFF",
+        // headerTitleStyle: { fontWeight: "bold" },
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="FirstPage" component={FirstPage} />
+      <Stack.Screen name="SecondPage" component={SecondPage} />
+      <Stack.Screen name="ThirdPage" component={ThirdPage} />
+    </Stack.Navigator>
+  );
+}
+
+function SecondScreenStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        // headerTintColor: "#FFFD9B",
+        // headerTitleStyle: { fontWeight: "bold" },
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="SecondPage" component={SecondPage} />
+      <Stack.Screen name="FirstPage" component={FirstPage} />
+      <Stack.Screen name="ThirdPage" component={ThirdPage} />
+    </Stack.Navigator>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "#FFE777",
+          width: 240,
+        },
+      }}
+
+      drawerContent={(props)=><CustomSideBarMenu{...props}/>}
+
+    >
+      <Drawer.Screen
+        name="FirstDrawer"
+        component={FirstScreenStack}
+        options={{ title: "FirstDrawer", drawerLabel: "First page Option" }}
+      />
+
+      <Drawer.Screen
+        name="SecondDrawer"
+        component={SecondScreenStack}
+        options={{ title: "SecondDrawer", drawerLabel: "Second page Option" }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
 
 const App = () => {
   return (
     <NavigationContainer>
-
-      <stack.Navigator initialRouteName='First' 
-        screenOptions={{
-        headerStyle : {backgroundColor: '#008b8b'},
-        headerTintColor:'#ffff',
-        headerTitleStyle:{fontWeight:'bold'}}}
-      > 
-
-      {/* <stack.Navigator initialRouteName='FirstPage'> */}
-
-        <stack.Screen name='Home' 
-        component={HomeScreen} 
-        options={{title: 'Overview '}}
-        initialParams={{ itemId: 42}}/> 
-        
-        <stack.Screen name='Details' component={DetailsScreen}/>
-
-        {/* <stack.Screen name='Pagename' component={functionName}/> options={{title: 'EditHeaderName'}}*/}
-
-      </stack.Navigator>
-
+      <MyDrawer/>
     </NavigationContainer>
   )
-} 
-
+}
 
 export default App
